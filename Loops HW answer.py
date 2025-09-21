22a6e5065076e2c620c5917c4adabe20fd4564c8
KristaHarville92 <A02682712@alabama.edu>
2025-09-21 10:47:40 -0500

    Add files via upload

---
+++ Loops HW answer.py
@@ -0,0 +1,14 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: /Users/kristaharville/Documents/Loops HW.py =============
+do you want a muffin or cupcake?muffin
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?cupcake
+do you want a muffin or cupcake?0
+muffins; 9 cupcakes: 3
