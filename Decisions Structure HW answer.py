76465813228000e910495947261a02e30841a8c1
KristaHarville92 <A02682712@alabama.edu>
2025-09-21 09:58:56 -0500

    Add files via upload

---
+++ Decisions Structure HW answer.py
@@ -0,0 +1,11 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+====== RESTART: /Users/kristaharville/Documents/Decisions Structures HW.py =====
+Enter the distance in miles 100
+Enter miles/hours speed limit 60
+Enter your average speed in miles/hours 80
+1.6666666666666667 1.25
+time taken at speed limit  100.00 minutes
+time taken at your speed  75.00 minutes
+time saved in min  25.00
